0a82efa38c8a97b9cc6730869d54a979545ed313
William Royle <roylewil16@grinnell.edu>
2014-11-23 23:09:00 -0600

    added fillBackToBackDates

--- src/csc207/HWNA/scheduler/UtilsSchedule.java
+++ src/csc207/HWNA/scheduler/UtilsSchedule.java
@@ -1,6 +1,7 @@
 package csc207.HWNA.scheduler;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class UtilsSchedule
 {
@@ -33,10 +34,14 @@ public class UtilsSchedule
   
   public static boolean canAdd(PairSchools theMatch, ScheduleDate theDate, Schedule addTo)
   {
+    if (!theMatch.home.canPlay(theDate) || !theMatch.away.canPlay(theDate))
+      {
+        return false;
+      }
     int foundDates = 0;
     int validAddition = 1;
     ArrayList<ScheduleDate> allDates = addTo.allDates;
-    ArrayList<Schedule.Game> gameList = addTo.gameList;
+    ArrayList<Game> gameList = addTo.gameList;
     for (int iter=0 ; iter<allDates.size() ; iter++)
       {
         if (allDates.get(iter).equals(theDate))
@@ -44,6 +49,7 @@ public class UtilsSchedule
             foundDates++;
           }
       }
+    
     for (int iter=0 ; iter<gameList.size() ; iter++)
       {
         PairSchools existantGamePair=gameList.get(iter).competing;
@@ -62,6 +68,77 @@ public class UtilsSchedule
     return (foundDates==1) && (validAddition==1);
   }
   
+  
+  public static Schedule fillBackToBackDates(ScheduleDate sat,ScheduleDate sun, ArrayList<PairSchools> theMatches, int numSchools)
+  {
+    ArrayList<Game> saturdayGames = new ArrayList<Game>();
+    ArrayList<Game> sundayGames = new ArrayList<Game>();
+    ArrayList<ScheduleDate> theDates = new ArrayList<ScheduleDate>();
+    theDates.add(sat);
+    theDates.add(sun);
+    Schedule Dummy=new Schedule(null,theDates,numSchools);
+    Random dice = new Random();
+    int firstPairNum = dice.nextInt(theMatches.size());
+    saturdayGames.add(new Game(theMatches.get(firstPairNum),sat));
+    School firstSchool=theMatches.get(firstPairNum).home;
+    School prevSchool=theMatches.get(firstPairNum).away;
+    boolean found;
+    for (int count=0;count<numSchools-2;count++)
+      {
+        found=false;
+        if (count % 2==0)//even, sunday
+          {
+            while (!found)
+              {
+                int pairNum = dice.nextInt(theMatches.size());
+                PairSchools toInsert = theMatches.get(pairNum);
+                if (toInsert.away.equals(prevSchool.name) && canAdd(toInsert,sun,Dummy))
+                  {
+                    found=true;
+                    sundayGames.add(new Game(toInsert,sun));
+                    prevSchool=toInsert.home;
+                  }
+              }
+            
+          }
+        else //saturday
+          {
+            while (!found)
+              {
+                int pairNum = dice.nextInt(theMatches.size());
+                PairSchools toInsert = theMatches.get(pairNum);
+                if (toInsert.home.equals(prevSchool.name) && canAdd(toInsert,sat,Dummy))
+                  {
+                    found=true;
+                    saturdayGames.add(new Game(toInsert,sat));
+                    prevSchool=toInsert.away;
+                  }
+              }
+          }
+        
+      }
+    found=false;
+    while (!found)
+      {
+        int pairNum = dice.nextInt(theMatches.size());
+        PairSchools toInsert = theMatches.get(pairNum);
+        if (toInsert.away.equals(prevSchool.name) && canAdd(toInsert,sun,Dummy)&&toInsert.home.equals(firstSchool.name))
+          {
+            found=true;
+            sundayGames.add(new Game(toInsert,sat));
+          }
+      }
+    Dummy.gameList.addAll(saturdayGames);
+    Dummy.gameList.addAll(sundayGames);
+    
+    
+    
+    
+    
+    return Dummy;
+  }
+  
+ 
   
   
   
