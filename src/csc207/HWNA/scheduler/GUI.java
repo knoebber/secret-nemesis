8c32d33d685840026e0cb1ea3ef36909c91f909b
knoebber <knoebber@gmail.com>
2014-12-02 18:59:14 -0600

    added boxes for trials and permutations

--- src/csc207/HWNA/scheduler/GUI.java
+++ src/csc207/HWNA/scheduler/GUI.java
@@ -3,12 +3,14 @@ package csc207.HWNA.scheduler;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
@@ -22,6 +24,7 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
 /**
  * @author Harry Baker
  * @author William Royle
@@ -34,6 +37,10 @@ import javafx.stage.Stage;
  * Allows the user to specify a file to use and when
  * to generate the schedule. 
  * 
+ * Got help from
+ * http://stackoverflow.com/questions/22627579/how-load-css-file-in-javafx8
+ * https://introjava.wordpress.com/2012/03/23/java-fx-2-linear-gradients/
+ * http://java-buddy.blogspot.com/2014/05/javafx-example-rectangle-filled-with.html
 */
 public class GUI
     extends Application
@@ -41,6 +48,40 @@ public class GUI
   File info;
   ArrayList<Date> dates;
   ArrayList<PairSchools> schools;
+  boolean showingSettings = false;
+  int trials = 10000;
+  int permutations = 3;
+
+  private TextField makeTextField(String prompt, int x, int y)
+  {
+    TextField field = new TextField(prompt);
+    field.setLayoutX(x);
+    field.setLayoutY(y);
+    field.setMaxWidth(75);
+    return field;
+  }
+
+  private Button makeButton(String name, int x, int y)
+  {
+    Button button = new Button(name);
+    button.setLayoutX(x);
+    button.setLayoutY(y);
+    button.scaleXProperty().set(1.5);
+    button.scaleYProperty().set(1.5);
+    button.getStyleClass().add("button");
+    return button;
+  }
+
+  private Text makeText(int x, int y)
+  {
+    Text text = new Text();
+    text.setLayoutX(x);
+    text.setLayoutY(y);
+    text.setFont(Font.font(null, FontWeight.EXTRA_LIGHT, 12));
+    text.setFill(Color.WHITE);
+    text.getStyleClass().add("text");
+    return text;
+  }
 
   @Override
   public void start(Stage primaryStage)
@@ -52,59 +93,57 @@ public class GUI
     //Pane allows for most basic positioning
     pane.setPadding(new Insets(25, 25, 25, 25));
     Scene scene = new Scene(pane, 500, 500);
-    scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-    //wasn't loading before, but this helped me solve the problem:
-    // http://stackoverflow.com/questions/22627579/how-load-css-file-in-javafx8
+    scene.getStylesheets().add(getClass().getResource("application.css")
+                                         .toExternalForm());
     pane.getStyleClass().add("root");
-    //note: used this example to use customization:
-    //https://introjava.wordpress.com/2012/03/23/java-fx-2-linear-gradients/
-    //LinearGradient thanks to http://java-buddy.blogspot.com/2014/05/javafx-example-rectangle-filled-with.html
-    Stop[] stops = new Stop[] {
-                new Stop(0, Color.BLACK),
-                new Stop(1, Color.DARKRED)
-    };
-    LinearGradient linearGradient = new LinearGradient(0, 1, 0, 0, true, CycleMethod.NO_CYCLE, stops);
-    
-    //LinearGradient rectangle
-    Rectangle rectangle = new Rectangle(420,250);
+
+    //Make the red to black gradient box
+    Stop[] stops =
+        new Stop[] { new Stop(0, Color.BLACK), new Stop(1, Color.DARKRED) };
+    LinearGradient linearGradient =
+        new LinearGradient(0, 1, 0, 0, true, CycleMethod.NO_CYCLE, stops);
+    Rectangle rectangle = new Rectangle(420, 250);
     rectangle.setLayoutY(20);
     rectangle.setLayoutX(40);
     rectangle.setFill(linearGradient);
     pane.getChildren().add(rectangle);
-    
-    
-    //make the open button
-    Button openResource = new Button("Open School Infomation File");
-    
-    openResource.setLayoutX(130);
-    openResource.setLayoutY(50);
-    openResource.getStyleClass().add("button");
 
-    //make the button bigger
-    openResource.scaleXProperty().set(1.5);
-    openResource.scaleYProperty().set(1.5);
-    //add button to pane
+    /*
+     * Make the buttons
+     */
+    Button openResource = makeButton("Open School Infomation File", 130, 50);
+    Button generate = makeButton("Generate Schedule", 170, 170);
+    Button help = makeButton("Need Help?", 350, 450);
+    Button settings = makeButton("More Settings", 60, 450);
+    pane.getChildren().add(settings);
     pane.getChildren().add(openResource);
-
-    //make generate button
-    Button generate = new Button("Generate Schedule");
-    generate.setLayoutY(180);
-    generate.setLayoutX(170);
-    generate.scaleXProperty().set(1.5);
-    generate.scaleYProperty().set(1.5);
     pane.getChildren().add(generate);
-    generate.getStyleClass().add("button");
-    
-    //make a "help" button
-    Button help = new Button("Need help?");
-    help.setLayoutY(450);
-    help.setLayoutX(350);
-    help.scaleXProperty().set(1.5);
-    help.scaleYProperty().set(1.5);
     pane.getChildren().add(help);
-    help.getStyleClass().add("button");
-    
-    //create a pop up from the help button
+
+    /*
+     * Make the texts
+     */
+    Text currentFile = makeText(50, 120);
+    currentFile.setText("Current File: ");
+    Text openedFile = makeText(50, 150);
+    Text finished = makeText(50, 385);
+    Text finalPath = makeText(50, 415);
+    Text permText = makeText(45,225);
+    Text trialText = makeText(380,225);
+    permText.setText("Permutations per trial");
+    trialText.setText("Trials");
+    pane.getChildren().add(currentFile);
+    pane.getChildren().add(openedFile);
+    pane.getChildren().add(finished);
+    pane.getChildren().add(finalPath);
+
+    /*
+     * Make the textfields
+     */
+    TextField permutationsField = makeTextField("3", 45, 230);
+    TextField trialsField = makeTextField("10000", 380, 230);
+
+    //create a pop up for the help button
     Pane helpPane = new Pane(); //the pane for the help window
     helpPane.setPadding(new Insets(25, 25, 25, 25));
     Stage helpWindow = new Stage();
@@ -116,60 +155,36 @@ public class GUI
     Image img = new Image("exampleschedule.PNG");
     ImageView imgView = new ImageView(img);
     helpPane.getChildren().add(imgView);
-    //imgView.
-    //helpPane.getStylesheets().add(getClass().getResource("popup.css").toExternalForm()); 
-    //TODO: write css for pop up
-    
-    
-    //label.getStylesheets().add(getClass().getResource("popup.css").toExternalForm());
-    //^causes errors
-    //label.getStyleClass().add("popup");
-    
-    //make a text
-    Text currentFile = new Text("Current File:");
-    currentFile.setX(50);
-    currentFile.setY(120);
-    currentFile.setFont(Font.font(null, FontWeight.EXTRA_LIGHT, 12));
-    currentFile.setFill(Color.WHITE);
-    pane.getChildren().add(currentFile);
 
-    //shows where we opened the file
-    Text openedFile = new Text();
-    openedFile.setX(50);
-    openedFile.setY(150);
-    openedFile.setFont(Font.font(null, FontWeight.EXTRA_LIGHT, 12));
-    openedFile.setFill(Color.WHITE);
-    pane.getChildren().add(openedFile);
-
-    //tells us when it generated the schedule
-    Text finished = new Text();
-    finished.setX(50);
-    finished.setY(385);
-    finished.setFont(Font.font(null, FontWeight.EXTRA_LIGHT, 12));
-    pane.getChildren().add(finished);
-    finished.setFill(Color.WHITE);
-    //shows where the output is
-    Text finalPath = new Text();
-    finalPath.setX(50);
-    finalPath.setY(415);
-    finalPath.setFont(Font.font(null, FontWeight.EXTRA_LIGHT, 12));
-    pane.getChildren().add(finalPath);
-    finalPath.getStyleClass().add("text");
-    finalPath.setFill(Color.WHITE);
-    
     openResource.setOnAction(new EventHandler<ActionEvent>()
       {
         @Override
         public void handle(ActionEvent event) //this is called on click
         {
           //make an object that can open a file
-          System.out.println("here");
           FileChooser fileChooser = new FileChooser();
           fileChooser.setTitle("Open Resource File");
           info = fileChooser.showOpenDialog(primaryStage);
-          if(info!=null)
+          if (info != null)
             openedFile.setText(info.getPath());
 
+        }//handle
+      });
+    permutationsField.setOnAction(new EventHandler<ActionEvent>()
+      {
+        @Override
+        public void handle(ActionEvent event)
+        {
+          permutations = Integer.valueOf(permutationsField.getText());
+        }//handle
+      });
+
+    trialsField.setOnAction(new EventHandler<ActionEvent>()
+      {
+        @Override
+        public void handle(ActionEvent event)
+        {
+          trials = Integer.valueOf(trialsField.getText());
         }//handle
       });
 
@@ -184,10 +199,13 @@ public class GUI
               try
                 {
                   Schedule originalSchedule = Parser.parse(info);
-                  UtilsSchedule.optimalFillBackToBack(originalSchedule, 0, 0);
-                  UtilsSchedule.optimalSchedule(originalSchedule,2000);
-                  //UtilsSchedule.basicFill(originalSchedule);
-                  ScheduleWriter.write(originalSchedule ,info.getParent()+"/schedule.txt");
+                  
+                  UtilsSchedule.makeSchedule(originalSchedule, trials,
+                                             permutations);
+                  
+                  ScheduleWriter.write(originalSchedule, info.getParent()
+                                                         + "/schedule.txt");
+                  
                   finished.setText("Wrote schedule:"); //tell user that it was wrote
                   finalPath.setText(info.getParent() + "/schedule.txt");
                 }//try
@@ -200,15 +218,39 @@ public class GUI
 
         }//handle
       });
-    
-    help.setOnAction(new EventHandler<ActionEvent>() 
-    {
+
+    help.setOnAction(new EventHandler<ActionEvent>()
+      {
         @Override
-        public void handle(ActionEvent event) {
+        public void handle(ActionEvent event)
+        {
           helpWindow.show();
-          
         }
-    });
+      });
+
+    settings.setOnAction(new EventHandler<ActionEvent>()
+      {
+        @Override
+        public void handle(ActionEvent event)
+        {
+          showingSettings = !showingSettings;
+          if (showingSettings)
+            {
+              pane.getChildren().add(permutationsField);
+              pane.getChildren().add(trialsField);
+              pane.getChildren().add(permText);
+              pane.getChildren().add(trialText);
+            }//if
+          else
+            {
+              pane.getChildren().remove(permutationsField);
+              pane.getChildren().remove(trialsField);
+              pane.getChildren().remove(permText);
+              pane.getChildren().remove(trialText);
+            }//else
+
+        }
+      });
 
     primaryStage.setScene(scene);
     primaryStage.show(); //show the scene
